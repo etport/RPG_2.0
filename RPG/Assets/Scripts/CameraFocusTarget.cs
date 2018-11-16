844c61c15f6bcc764e1bcac9a53049c50c4264d5
Emily Porterfield <emilyporterfield13@gmail.com>
2018-11-16 15:25:18 -0800

    Revert "Revert "Scene Setup""
    This reverts commit 6e6165644dc938646de27f97af07eea9eda6edc8.

---
+++ RPG/Assets/Scripts/CameraFocusTarget.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UMA;
+using UMA.CharacterSystem;
+
+
+public class CameraFocusTarget : MonoBehaviour {
+
+
+    public DynamicCharacterAvatar target;
+    Animator animator;
+
+    // Use this for initialization
+    void Start ()
+    {
+        animator = target.GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (gameObject.GetComponent<Camera>().isActiveAndEnabled)
+        {
+            Transform head = animator.GetBoneTransform(HumanBodyBones.Head);
+            transform.LookAt(head);
+
+        }
+    }
+}
